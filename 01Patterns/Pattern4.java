b73d78fd22786b10230df061b7e9696e14af390d
Priyanshu Kumar <102889190+priyanshuv-raw@users.noreply.github.com>
2023-04-28 08:00:52 +0530

    Update Pattern4.java

--- 01Patterns/Pattern4.java
+++ 01Patterns/Pattern4.java
@@ -1,3 +1,27 @@
-public class Pattern4 {
-    
+    /*
+    * * * * * *
+    *       *
+    *     *
+    *   *
+    * *
+    *
+ */
+
+ import java.util.*;
+ class Pattern4 {
+     public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+         System.out.print("Enter the number of lines for pattern:");
+         int lines = input.nextInt();
+         int row,col =1;
+         for (row = 1; row <= lines; row++) {
+            for (col = 1; col <= lines; col++) {
+                if(row == 1 || col == 1 || (row+col)==lines)
+                    System.out.print("* ");
+            }
+            System.out.println();
+         }
+         input.close();
+     }
+ }
 }
