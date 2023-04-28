aee2b0d7cda1ed491e8cd7ab3df6d990f8caa372
priyanshuv-raw <priyanshu.vraw>
2023-04-28 21:49:47 +0530

    updated

--- 01Patterns/Pattern4.java
+++ 01Patterns/Pattern4.java
@@ -14,14 +14,17 @@
          System.out.print("Enter the number of lines for pattern:");
          int lines = input.nextInt();
          int row,col =1;
-         for (row = 1; row <= lines; row++) {
-            for (col = 1; col <= lines; col++) {
-                if(row == 1 || col == 1 || (row+col)==lines)
-                    System.out.print("* ");
+         for (row = 0; row < lines; row++) {
+            for (col = 0; col < lines; col++) {
+                if(row == 0 || col == 0 || (row+col)==lines-1){
+                    System.out.print("* ");}
+                else{
+                    System.out.print("  ");
+                }
+
             }
             System.out.println();
          }
          input.close();
      }
  }
-}
--- 01Patterns/Pattern5.java
+++ 01Patterns/Pattern5.java
@@ -5,11 +5,24 @@
     7 8 9 10
  */
 
+import java.util.Scanner;
+
 /**
  * Pattern5
  */
 
 public class Pattern5 {
-
-    
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the pattern:");
+        int lines = Sc.nextInt();
+        int count=1,row,col=0;
+        for (row = 0; row < lines; row++) {
+            for (col = 0; col < row; col++) {
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }    
 }
